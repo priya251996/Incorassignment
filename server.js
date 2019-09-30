9dedda0b8d4b8e93da2fc57aa6a9714396c49396
priya251996 <priya.b.shyna96@gmail.com>
2019-09-30 15:29:15 +0530

    My first commit

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express'),
 
 const app = express();
 
-app.use(express.static('./dist/quiz'));
+app.use(express.static('.quiz'));
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/quiz/index.html'));
+    res.sendFile(path.join(__dirname, 'quiz/index.html'));
 
 });
 app.listen(process.env.PORT || 8080, () => {
